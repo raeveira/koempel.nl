2ce109e6bbbf78cc48354a2d69e63020bf3eb0d9
raeveira <rae@raeveira.nl>
2024-09-10 14:20:45 +0200

    renamed english to dutch

--- components/News.tsx
+++ components/News.tsx
@@ -5,12 +5,14 @@ import Link from 'next/link'
 
 
 export default function NewsComponent() {
-    return (
-        <section className="py-12 bg-gray-100">
-          <div className="container mx-auto px-4">
-            <h2 className="text-3xl font-bold mb-6">Latest News</h2>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-              {latestNews.map((news, index) => (
+  return (
+    <section className="py-12 bg-gray-100">
+      <div className="container mx-auto px-4">
+        <h2 className="text-3xl font-bold mb-6">Laatste nieuws</h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {latestNews.map((news, index) => (
+            <>
+              {news.title != '' && (
                 <Card key={index}>
                   <CardHeader>
                     <CardTitle>{news.title}</CardTitle>
@@ -20,9 +22,11 @@ export default function NewsComponent() {
                     <Button className="mt-4" variant="outline"><Link href={news.href}>Read More</Link></Button>
                   </CardContent>
                 </Card>
-              ))}
-            </div>
-          </div>
-        </section>
-    )
+              )}
+            </>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
 }
